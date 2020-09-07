e0a9f3e2d07686cfd8e539cb2291382c9034032f
adamkhairi <57996941+adamkhairi@users.noreply.github.com>
2020-09-07 21:16:16 +0100

    UI Optimise

--- resources/views/experience/experiences.blade.php
+++ resources/views/experience/experiences.blade.php
@@ -8,9 +8,54 @@
 @section('content')
     <div class="p-16"></div>
 
-    <section class="min-h-screen">
+    <section class="blog text-gray-700 body-font">
+        <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
+                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900"> Blog</h1>
+                <p class="lg:w-1/2 w-full leading-relaxed text-base">
+                    J'aime bien partager mes connaissances et des recherche intéressantes, pour le faire j'ai mis en
+                    place un blog personnel.
+                    Nous abordons plusieurs sujets intéressants et je donne quelques astuces et conseils aux jeunes
+                    développeurs pour mieux s'en sortir. </p>
+            </div>
+            <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+
+                @foreach($exper as $item)
+
+                    <div class="p-4 md:w-1/3 md:mb-0 mb-6 flex flex-col justify-center items-center max-w-sm mx-auto">
+                        <div class="bg-gray-300 h-56 w-full rounded-lg shadow-md bg-cover bg-center"
+                             style="background-image: url(https://images.unsplash.com/photo-1521185496955-15097b20c5fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80)"></div>
+
+                        <div class=" w-70 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden p-5">
+
+                            <div class="header-content w-full inline-flex justify-center items-center">
+                                <div class="category-badge  h-8 w-8 m rounded-full m-1 bg-purple-100">
+                                    <div class="h-6 w-6 rounded-full m-1 bg-purple-500 overflow-hidden object-cover">
+
+                                        <img src="{{ $item->logo }}" alt="">
+
+                                    </div>
+                                </div>
+
+                                <h1 class="category-title font-bold flex-1 w-full mb-3 whitespace-no-wrap text-sm text-center">
+                                    {{ $item->company }}
+                                </h1>
+                            </div>
+                            <div class="title-post font-medium mb-1">
+                                {{ $item->post }}
+                            </div>
+                            <div class="summary-post text-base text-justify">
+                                {{ $item->description }}
+                            </div>
+
+                        </div>
+                    </div>
 
 
+                @endforeach
+
+            </div>
+        </div>
     </section>
 @endsection
 
--- resources/views/work/works.blade.php
+++ resources/views/work/works.blade.php
@@ -6,111 +6,122 @@
 @endsection
 
 @section('content1')
-    <section class="min-h-screen">
+    <div class="container flex mx-auto pt-16">
 
-        <div class="p-16"></div>
-        <section class="bg-white py-8">
 
-            <div class=" w-full lg:w-4/5 mx-auto flex items-center flex-wrap pt-4 pb-12">
+        <section class="w-full md:w-2/3 flex flex-col items-center px-3 pt-4 text-black">
 
-                <nav id="store" class="w-full z-30 top-0 px-6 py-1">
-                    <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3">
 
-                        <a class="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-3xl "
-                           href="#">
+            <section class="bg-white ">
+                <div class=" w-full lg:w-4/5 mx-auto flex items-center flex-wrap pt-4 pb-12">
 
-                            <h1 class="mb-8">My work</h1>
-                        </a>
+                    <nav id="store" class="w-full z-30 top-0 px-6 py-1">
+                        <div
+                            class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3">
 
-                        <div class="flex items-center" id="store-nav-content">
-                            @auth()
-                                <a class="pl-3 inline-block no-underline hover:text-black"
-                                   href="{{ route('work.create') }}">
-                                    Add New
-                                </a>
-                            @endauth
+                            <a class="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-3xl "
+                               href="#">
 
-                            <a class="pl-3 inline-block no-underline hover:text-black" href="#">
-                                <svg class="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg"
-                                     width="24"
-                                     height="24" viewBox="0 0 24 24">
-                                    <path
-                                        d="M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10 c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z"/>
-                                </svg>
+                                <h1 class="mb-8">My work</h1>
                             </a>
 
-                        </div>
-                    </div>
-                </nav>
-                <div class="flex flex-wrap -mx-1 lg:-mx-4">
+                            <div class="flex items-center" id="store-nav-content">
+                                @auth()
+                                    <a class="pl-3 inline-block no-underline hover:text-black"
+                                       href="{{ route('work.create') }}">
+                                        Add New
+                                    </a>
+                                @endauth
 
-
-                    @foreach($works as $work)
-                        <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3 ">
-
-                            {{--                    <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col tracking-wide">--}}
-                            {{--                        <a href="{{ route('works.show', $work->id) }}">--}}
-                            {{--                            <img class="hover:grow hover:shadow-lg"--}}
-                            {{--                                 src="{{ $work->img }}">--}}
-                            {{--                            <div class="pt-3 flex items-center justify-between">--}}
-                            {{--                                <p class="">{{ $work->name }}</p>--}}
-
-                            {{--                            </div>--}}
-                            {{--                            <p class="pt-1 text-gray-900">--}}
-                            {{--                                {{ $work->description }}--}}
-                            {{--                            </p>--}}
-                            {{--                        </a>--}}
-                            {{--                    </div>--}}
-
-                            <article class="overflow-hidden rounded-lg shadow-lg">
-
-                                <a href="{{ route('works.show', $work->id) }}">
-                                    <img alt="Placeholder" class="block h-auto w-full"
-                                         src="{{ $work->img }}">
+                                <a class="pl-3 inline-block no-underline hover:text-black" href="#">
+                                    <svg class="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg"
+                                         width="24"
+                                         height="24" viewBox="0 0 24 24">
+                                        <path
+                                            d="M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10 c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z"/>
+                                    </svg>
                                 </a>
 
-                                <header class="flex items-center justify-between leading-tight p-2 md:p-4">
-                                    <h1 class="text-lg">
-                                        <a class="no-underline hover:underline text-black"
-                                           href="{{ route('works.show', $work->id) }}">
-                                            {{ $work->name }}
-                                        </a>
-                                    </h1>
-                                    <p class="text-grey-darker text-sm">
-                                        11/1/19
-                                    </p>
-                                </header>
-
-                                <footer class="flex items-center justify-between leading-none p-2 md:p-4">
-                                    <a class="flex items-center no-underline hover:underline text-black"
-                                       href="{{ route('works.show', $work->id) }}">
-                                        <img alt="Placeholder" class="block rounded-full"
-                                             src="https://picsum.photos/32/32/?random">
-                                        <p class="ml-2 text-sm">
-                                            {{$work->description}}
-                                        </p>
-                                    </a>
-                                    <a class="no-underline text-grey-darker hover:text-red-dark" href="#">
-                                        <span class="hidden">Like</span>{{ $work->rating }}
-                                        <i class="fa fa-heart"></i>
-                                    </a>
-                                </footer>
-
-                            </article>
-
+                            </div>
                         </div>
-                    @endforeach
-                </div>
+                    </nav>
+                    <div class="flex flex-wrap w-full -mx-1 lg:-mx-4">
 
-            </div>
+
+                        @foreach($works as $work)
+                            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/2 ">
+
+                                <article class="overflow-hidden rounded-lg shadow-lg">
+
+                                    <a href="{{ route('works.show', $work->id) }}">
+                                        <img alt="Placeholder" class="block h-auto w-full"
+                                             src="{{ $work->img }}">
+                                    </a>
+
+                                    <header class="flex items-center justify-between leading-tight p-2 md:p-4">
+                                        <h1 class="text-lg">
+                                            <a class="no-underline hover:underline text-black"
+                                               href="{{ route('works.show', $work->id) }}">
+                                                {{ $work->name }}
+                                            </a>
+                                        </h1>
+                                        <p class="text-grey-darker text-sm">
+                                            11/1/19
+                                        </p>
+                                    </header>
+
+                                    <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+                                        <a class="flex items-center no-underline hover:underline text-black"
+                                           href="{{ route('works.show', $work->id) }}">
+                                            <img alt="Placeholder" class="block rounded-full"
+                                                 src="https://picsum.photos/32/32/?random">
+                                            <p class="ml-2 text-sm">
+                                                {{$work->description}}
+                                            </p>
+                                        </a>
+                                        <a class="no-underline text-grey-darker hover:text-red-dark" href="#">
+                                            <span class="hidden">Like</span>{{ $work->rating }}
+                                            <i class="fa fa-heart"></i>
+                                        </a>
+                                    </footer>
+
+                                </article>
+
+                            </div>
+                        @endforeach
+                    </div>
+
+                </div>
+                {{--PAGINATION--}}
+                <div class="bg-gray-200">
+                    {{ $works->links() }}
+                </div>
+            </section>
+
 
         </section>
-        {{--PAGINATION--}}
-        <div class="bg-gray-200">
-            {{ $works->links() }}
-        </div>
+        <aside class="w-full md:w-1/3 flex flex-col items-center px-3 text-black">
 
-    </section>
+            <div class="w-full bg-white shadow flex flex-col my-4 p-6">
+                <p class="text-xl font-semibold pb-5">About Us</p>
+                <p class="pb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mattis est eu odio
+                    sagittis tristique. Vestibulum ut finibus leo. In hac habitasse platea dictumst.</p>
+                <a href="#"
+                   class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
+                    Get to know us
+                </a>
+            </div>
+
+{{--            <div class="w-full bg-white shadow flex flex-col my-4 p-6">--}}
+
+{{--                <a href="#"--}}
+{{--                   class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-6">--}}
+{{--                    <i class="fab fa-instagram mr-2"></i> Follow @dgrzyb--}}
+{{--                </a>--}}
+{{--            </div>--}}
+
+        </aside>
+    </div>
 @endsection
 
 
